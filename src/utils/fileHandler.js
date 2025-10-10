e46dccca48f93574ac161003760e0e54a50c6c8b
Jared Nielsen <jnielsen@splunk.com>
2025-10-10 09:05:16 -0400

    add recursive PDF generation for subfolder

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -1,6 +1,57 @@
 import path from "path";
 import fs from "fs/promises";
 
+/**
+ * Find all readme files to process based on recursive option
+ * @param {string} sourceDir - Source directory
+ * @param {boolean} recursive - Whether to include supplemental folder
+ * @returns {Array} Array of file objects with path and relative path info
+ */
+export async function findReadmeFiles(sourceDir, recursive = false) {
+  const files = [];
+
+  // Find main readme file in source directory
+  const mainFiles = await fs.readdir(sourceDir);
+  const mainReadmeFile = mainFiles.find((file) => /-readme\.md$/i.test(file));
+
+  if (mainReadmeFile) {
+    files.push({
+      fullPath: path.join(sourceDir, mainReadmeFile),
+      fileName: mainReadmeFile,
+      directory: sourceDir,
+      isSupplemental: false,
+    });
+  }
+
+  // If recursive, also check supplemental subfolder
+  if (recursive) {
+    const supplementalDir = path.join(sourceDir, "supplemental");
+
+    try {
+      const supplementalStats = await fs.stat(supplementalDir);
+      if (supplementalStats.isDirectory()) {
+        const supplementalFiles = await fs.readdir(supplementalDir);
+        const supplementalReadmeFiles = supplementalFiles.filter((file) =>
+          /-readme\.md$/i.test(file)
+        );
+
+        for (const file of supplementalReadmeFiles) {
+          files.push({
+            fullPath: path.join(supplementalDir, file),
+            fileName: file,
+            directory: supplementalDir,
+            isSupplemental: true,
+          });
+        }
+      }
+    } catch (err) {
+      // Supplemental directory doesn't exist or isn't accessible, that's okay
+    }
+  }
+
+  return files;
+}
+
 export function buildOutputFilename(metadata, sourceDir) {
   try {
     let { course_id, course_title } = metadata;
